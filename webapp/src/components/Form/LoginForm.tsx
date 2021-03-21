be08471d7c8c161aed3c135eb1413c3367c42a32
Piero Narciso <pieronarciso988@gmail.com>
2021-03-21 15:59:30 -0500

    Added template to Login Form

--- webapp/src/components/Form/LoginForm.tsx
+++ webapp/src/components/Form/LoginForm.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PInput from '@/components/base/PInput';
+import PBtn from '@/components/base/PBtn';
 
 const LoginForm: React.FC<
   React.DetailedHTMLProps<React.HTMLProps<HTMLDivElement>, HTMLDivElement>
 > = ({ className, ...props }) => {
+  // Username State
+  const [username, setUsername] = useState('');
+  /** Set username to new value in `Input` Change */
+  const usernameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(username);
+  }
+
   return (
-    <div className={'shadow rounded' + ' ' + className} {...props}>
-      <p>Login</p>
-      <PInput />
+    <div
+      className={'bg-white shadow rounded px-2 py-3' + ' ' + className}
+      {...props}
+    >
+      <p className="text-lg font-semibold">Login</p>
+      <form className="mt-4" onSubmit={submitHandler}>
+        <PInput
+          placeholder="Enter an Username"
+          value={username}
+          onChange={usernameHandler}
+        />
+        <div className="flex flex-row-reverse mt-3">
+          <PBtn className="text-sm">Log In</PBtn>
+        </div>
+      </form>
     </div>
   );
 };
